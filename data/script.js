94690da226ce3eedea13bba08563872669365440
Puja Erawan <puja.erawan@viewtrixtech.com>
2023-02-18 13:30:03 +0700

    combine all sensor readings event

--- data/script.js
+++ data/script.js
@@ -1,113 +1,124 @@
 /*
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
+  Complete project details at 
+  https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  Permission is hereby granted, free of charge, to any person obtaining a copy 
+  of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in 
+  all copies or substantial portions of the Software.
 */
 
-let scene, camera, rendered, cube;
+let scene, camera, cube;
 
-function parentWidth(elem) {
-  return elem.parentElement.clientWidth;
+/* functions prototypes ------------------------------------------------------*/
+function rad2deg (rad, precision = 2) 
+{
+    return (parseFloat(rad) * (180 / Math.PI)).toFixed(precision);
+};
+
+function resetPosition(elem)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/"+elem.id, true);
+    xhr.send();
+  }
+
+function parentSize(elem) 
+{
+    return {
+        w: elem.parentElement.clientWidth,
+        h: elem.parentElement.clientHeight,
+    }
 }
 
-function parentHeight(elem) {
-  return elem.parentElement.clientHeight;
-}
+function init3D()
+{
+    elem3d = document.getElementById("3Dcube");
+    const { w, h } = parentSize(elem3d);
 
-function init3D(){
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffffff);
+    // create cube
+    const material = new THREE.MeshFaceMaterial([
+        new THREE.MeshBasicMaterial({color:0x03045e}),
+        new THREE.MeshBasicMaterial({color:0x023e8a}),
+        new THREE.MeshBasicMaterial({color:0x0077b6}),
+        new THREE.MeshBasicMaterial({color:0x03045e}),
+        new THREE.MeshBasicMaterial({color:0x023e8a}),
+        new THREE.MeshBasicMaterial({color:0x0077b6}),
+    ]);
+    const geometry = new THREE.BoxGeometry(6, 1, 4);
+    cube = new THREE.Mesh(geometry, material);
 
-  camera = new THREE.PerspectiveCamera(75, parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube")), 0.1, 1000);
+    // create axis 
+    const axesHelper = new THREE.AxesHelper( 5 );
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+    // setup scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    
+    // combine object into scene
+    scene.add(axesHelper);
+    scene.add(cube);
 
-  document.getElementById('3Dcube').appendChild(renderer.domElement);
+    // setup camera
+    camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+    camera.position.z = 5;
 
-  // Create a geometry
-  const geometry = new THREE.BoxGeometry(5, 1, 4);
+    // setup renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(w, h);
+    renderer.render(scene, camera);
 
-  // Materials of each face
-  var cubeMaterials = [
-    new THREE.MeshBasicMaterial({color:0x03045e}),
-    new THREE.MeshBasicMaterial({color:0x023e8a}),
-    new THREE.MeshBasicMaterial({color:0x0077b6}),
-    new THREE.MeshBasicMaterial({color:0x03045e}),
-    new THREE.MeshBasicMaterial({color:0x023e8a}),
-    new THREE.MeshBasicMaterial({color:0x0077b6}),
-  ];
-
-  const material = new THREE.MeshFaceMaterial(cubeMaterials);
-
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  camera.position.z = 5;
-  renderer.render(scene, camera);
+    elem3d.appendChild(renderer.domElement);
 }
 
 // Resize the 3D object when the browser window changes size
-function onWindowResize(){
-  camera.aspect = parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube"));
-  //camera.aspect = window.innerWidth /  window.innerHeight;
-  camera.updateProjectionMatrix();
-  //renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+window.addEventListener('resize', function() {
+    const { w, h } = parentSize(document.getElementById("3Dcube"));
 
-}
-
-window.addEventListener('resize', onWindowResize, false);
+    camera.aspect = w / h;
+    //camera.aspect = window.innerWidth /  window.innerHeight;
+    camera.updateProjectionMatrix();
+    //renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(w, h);
+}, false);
 
 // Create the 3D representation
 init3D();
 
 // Create events for the sensor readings
-if (!!window.EventSource) {
-  var source = new EventSource('/events');
+if (!!window.EventSource) 
+{
+    var source = new EventSource('/events');
 
-  source.addEventListener('open', function(e) {
-    console.log("Events Connected");
-  }, false);
+    source.addEventListener('open', function(e) {
+        console.log("Events Connected");
+    }, false);
 
-  source.addEventListener('error', function(e) {
-    if (e.target.readyState != EventSource.OPEN) {
-      console.log("Events Disconnected");
-    }
-  }, false);
+    source.addEventListener('error', function(e) {
+        if (EventSource.OPEN != e.target.readyState) 
+        {
+            console.log("Events Disconnected");
+        }
+    }, false);
 
-  source.addEventListener('gyro_readings', function(e) {
-    //console.log("gyro_readings", e.data);
-    var obj = JSON.parse(e.data);
-    document.getElementById("gyroX").innerHTML = obj.gyroX;
-    document.getElementById("gyroY").innerHTML = obj.gyroY;
-    document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+    source.addEventListener('readings', function(e) {
+        var obj = JSON.parse(e.data);
+        document.getElementById("temp").innerHTML  = obj.temp;
+        document.getElementById("gyroX").innerHTML = obj.gyroX;
+        document.getElementById("gyroY").innerHTML = obj.gyroY;
+        document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+        document.getElementById("acclX").innerHTML = obj.acclX;
+        document.getElementById("acclY").innerHTML = obj.acclY;
+        document.getElementById("acclZ").innerHTML = obj.acclZ;
+        document.getElementById("tiltYaw").innerHTML   = rad2deg(obj.tiltYaw);
+        document.getElementById("tiltRoll").innerHTML  = rad2deg(obj.tiltRoll);
+        document.getElementById("tiltPitch").innerHTML = rad2deg(obj.tiltPitch);
 
-    // Change cube rotation after receiving the readinds
-    cube.rotation.x = obj.gyroY;
-    cube.rotation.z = obj.gyroX;
-    cube.rotation.y = obj.gyroZ;
-    renderer.render(scene, camera);
-  }, false);
-
-  source.addEventListener('temperature_reading', function(e) {
-    console.log("temperature_reading", e.data);
-    document.getElementById("temp").innerHTML = e.data;
-  }, false);
-
-  source.addEventListener('accelerometer_readings', function(e) {
-    console.log("accelerometer_readings", e.data);
-    var obj = JSON.parse(e.data);
-    document.getElementById("accX").innerHTML = obj.accX;
-    document.getElementById("accY").innerHTML = obj.accY;
-    document.getElementById("accZ").innerHTML = obj.accZ;
-  }, false);
-}
-
-function resetPosition(element){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/"+element.id, true);
-  console.log(element.id);
-  xhr.send();
+        // Change cube rotation after receiving the readinds
+        cube.rotation.z = obj.tiltRoll;
+        cube.rotation.x = obj.tiltPitch;
+        cube.rotation.y = obj.tiltYaw;
+        renderer.render(scene, camera);
+    }, false);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 /*********
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
+  Complete project details at 
+  https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
@@ -8,259 +9,314 @@
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
 *********/
-
+#include "main.h"
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Adafruit_SSD1306.h>
 #include <Arduino_JSON.h>
 #include "SPIFFS.h"
 
-// Replace with your network credentials
-const int port = SRVR_PORT;
-const char* ssid = SSID_NAME;
-const char* password = SSID_PASSWORD;
+/* private macros ------------------------------------------------------------*/
+#define RESULTANT(X,Y)  (sqrt((X)*(X) + (Y)*(Y)))
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(port);
+#define LED_PIN         2U
+#define CALIB_CNT       500U
+#define MEASURE_MS      1U
+#define REPORT_MS       250U
+#define TAU             0.98f
+#define YAW_THRES       0.03f
 
-// Create an Event Source on /events
-AsyncEventSource events("/events");
-
-// Json Variable to Hold Sensor Readings
-JSONVar readings;
-
-// Timer variables
-unsigned long lastTime = 0;  
-unsigned long lastTimeTemperature = 0;
-unsigned long lastTimeAcc = 0;
-unsigned long gyroDelay = 10;
-unsigned long temperatureDelay = 1000;
-unsigned long accelerometerDelay = 200;
-
-// Create a sensor object
+/* private variables ---------------------------------------------------------*/
+Adafruit_SSD1306 oled(128, 32, &Wire);
+AsyncWebServer server(SVR_PORT);
+AsyncEventSource event(SVR_EVT);
 Adafruit_MPU6050 mpu;
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
-sensors_event_t a, g, temp;
+sensors_vec_t tilt;
+sImu_t imu;
 
-float gyroX, gyroY, gyroZ;
-float accX, accY, accZ;
-float temperature;
+uint64_t u64_lastMeasure;
+uint64_t u64_lastReport;
 
-//Gyroscope sensor deviation
-float gyroXerror = 0.07;
-float gyroYerror = 0.03;
-float gyroZerror = 0.01;
+/* private functions delcarations --------------------------------------------*/
+void errorHandler(const char* err);
+void log(const char *msg);
+void initOLED();
+void initMPU();
+void initSPIFFS();
+void initWiFi();
 
-// Init MPU6050
-void initDisplay() {
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ; // Don't proceed, loop forever
-  }
-  display.display();
-  delay(500); // Pause for 2 seconds
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setRotation(90);
+void measure(sImu_t* p_imu);
+void calibrate(sImu_t* p_imu, uint32_t u32_sample);
+void calcTilt(sensors_vec_t* p_tilt, const sImu_t* p_imu, float f_dt);
+String convJSON(const sensors_vec_t* p_tilt, const sImu_t* p_imu);
+void report(const sensors_vec_t* p_tilt, const sImu_t* p_imu);
+
+/* public functions ----------------------------------------------------------*/
+void setup() 
+{
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+
+    initSPIFFS();
+    initOLED();
+    initMPU();
+    initWiFi();
+
+    // Handle Web Server Events
+    event.onConnect([](AsyncEventSourceClient *client) {
+        if (client->lastId())
+        {
+            Serial.printf("Client %u reconnected!\n", client->lastId());
+        }
+        client->send("hello!", NULL, millis(), 10000);
+    });
+
+    // Handle Web Server
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request) {
+        memset(&tilt, 0x0, sizeof(sensors_vec_t));
+        request->send(200, "text/plain", "OK");
+    });
+    server.on("/reset-yaw", HTTP_GET, [](AsyncWebServerRequest *request) {
+        tilt.heading = 0;
+        request->send(200, "text/plain", "OK");
+    });
+    server.on("/reset-roll", HTTP_GET, [](AsyncWebServerRequest *request) {
+        tilt.roll = 0;
+        request->send(200, "text/plain", "OK");
+    });
+    server.on("/reset-pitch", HTTP_GET, [](AsyncWebServerRequest *request) {
+        tilt.pitch = 0;
+        request->send(200, "text/plain", "OK");
+    });
+
+    server.serveStatic("/", SPIFFS, "/");
+    server.addHandler(&event);
+    server.begin();
 }
 
-void initMPU(){
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
+void loop() 
+{
+    uint32_t u32_ms;
+
+    // measure from sensor
+    u32_ms = millis() - u64_lastMeasure;
+    if (MEASURE_MS < u32_ms)
+    {
+        u64_lastMeasure = millis();
+        measure(&imu);
+        calcTilt(&tilt, &imu, (u32_ms * 0.001));
     }
-  }
-  Serial.println("MPU6050 Found!");
-}
 
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {
-    Serial.println("An error has occurred while mounting SPIFFS");
-  }
-  Serial.println("SPIFFS mounted successfully");
-}
-
-// Initialize WiFi
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
-  Serial.println("");
-  Serial.println(WiFi.localIP());
-}
-
-String getGyroReadings(){
-  mpu.getEvent(&a, &g, &temp);
-  float gyroX_temp = g.gyro.x;
-  if(abs(gyroX_temp) > gyroXerror)  {
-    gyroX += gyroX_temp/50.00;
-  }
-  
-  float gyroY_temp = g.gyro.y;
-  if(abs(gyroY_temp) > gyroYerror) {
-    gyroY += gyroY_temp/70.00;
-  }
-
-  float gyroZ_temp = g.gyro.z;
-  if(abs(gyroZ_temp) > gyroZerror) {
-    gyroZ += gyroZ_temp/90.00;
-  }
-
-  readings["gyroX"] = String(gyroX);
-  readings["gyroY"] = String(gyroY);
-  readings["gyroZ"] = String(gyroZ);
-
-  String jsonString = JSON.stringify(readings);
-  return jsonString;
-}
-
-String getAccReadings() {
-  mpu.getEvent(&a, &g, &temp);
-  // Get current acceleration values
-  accX = a.acceleration.x;
-  accY = a.acceleration.y;
-  accZ = a.acceleration.z;
-  readings["accX"] = String(accX);
-  readings["accY"] = String(accY);
-  readings["accZ"] = String(accZ);
-  String accString = JSON.stringify (readings);
-  return accString;
-}
-
-String getTemperature(){
-  mpu.getEvent(&a, &g, &temp);
-  temperature = temp.temperature;
-  return String(temperature);
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  initDisplay();
-  initWiFi();
-  initSPIFFS();
-  initMPU();
-
-  // print ip address
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("Viewtrix Technology");
-  display.println("");
-  display.print(WiFi.localIP());
-  display.print(":");
-  display.print(port, 1);
-  display.println("");
-  display.display();
-
-  // Handle Web Server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-
-  server.serveStatic("/", SPIFFS, "/");
-
-  server.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request){
-    gyroX=0;
-    gyroY=0;
-    gyroZ=0;
-    request->send(200, "text/plain", "OK");
-  });
-
-  server.on("/resetX", HTTP_GET, [](AsyncWebServerRequest *request){
-    gyroX=0;
-    request->send(200, "text/plain", "OK");
-  });
-
-  server.on("/resetY", HTTP_GET, [](AsyncWebServerRequest *request){
-    gyroY=0;
-    request->send(200, "text/plain", "OK");
-  });
-
-  server.on("/resetZ", HTTP_GET, [](AsyncWebServerRequest *request){
-    gyroZ=0;
-    request->send(200, "text/plain", "OK");
-  });
-
-  // Handle Web Server Events
-  events.onConnect([](AsyncEventSourceClient *client){
-    if(client->lastId()){
-      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    // report to client
+    u32_ms = millis() - u64_lastReport;
+    if (REPORT_MS < u32_ms)
+    {
+        u64_lastReport = millis();
+        report(&tilt, &imu);
     }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
-    client->send("hello!", NULL, millis(), 10000);
-  });
-  server.addHandler(&events);
-
-  server.begin();
 }
 
-void loop() {
-  bool displayUpdate = false;
+/* private functions definitions ---------------------------------------------*/
+void errorHandler(const char* err) 
+{
+    log(err);
 
-  if ((millis() - lastTime) > gyroDelay) {
-    displayUpdate = true;
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
-    lastTime = millis();
-  }
+    while(1) 
+    {
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(100);
+    };
+}
 
-  if ((millis() - lastTimeAcc) > accelerometerDelay) {
-    displayUpdate = true;
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getAccReadings().c_str(),"accelerometer_readings",millis());
-    lastTimeAcc = millis();
-  }
+void log(const char *msg)
+{
+    Serial.println(msg);
+    oled.println(msg);
+    oled.display();
+}
 
-  if ((millis() - lastTimeTemperature) > temperatureDelay) {
-    // Send Events to the Web Server with the Sensor Readings
-    events.send(getTemperature().c_str(),"temperature_reading",millis());
-    lastTimeTemperature = millis();
-  }
+void initOLED() 
+{
+    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+    { 
+        errorHandler("OLED (SSD1306) not found!");
+    }
 
-#if 0
-  if (displayUpdate) {
-    display.clearDisplay();
-    display.setCursor(0, 0);
+    // configure oled
+    oled.setTextSize(1);
+    oled.setTextColor(WHITE);
+    oled.setRotation(90);
+    oled.clearDisplay();
+    // welcome screen
+    log("Viewtrix Technology");
+}
 
-    display.print("IP: ");
-    display.print(WiFi.localIP());
-    display.println("");
+void initMPU()
+{
+    if (!mpu.begin()) 
+    {
+        errorHandler("IMU (MPU6050) not found!");
+    }
 
-    display.print("A: ");
-    display.print(a.acceleration.x, 1);
-    display.print(", ");
-    display.print(a.acceleration.y, 1);
-    display.print(", ");
-    display.print(a.acceleration.z, 1);
-    display.println("");
+    log("Calibrating...");
+    calibrate(&imu, CALIB_CNT);
+}
 
-    display.print("G: ");
-    display.print(g.gyro.x, 1);
-    display.print(", ");
-    display.print(g.gyro.y, 1);
-    display.print(", ");
-    display.print(g.gyro.z, 1);
-    display.println("");
+void initSPIFFS() 
+{
+    if (!SPIFFS.begin()) 
+    {
+        errorHandler("Web storage (SPIFFS) error!");
+    }
+}
 
-    display.print("T: ");
-    display.print(temp.temperature, 1);
-    display.println("");
+void initWiFi() 
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID_NAME, SSID_PASS);
 
-    display.display();
-  }
-#endif
+    log("Connecting...");
+    while (WL_CONNECTED != WiFi.status()) 
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println("");
+    log("Connected");
+}
+
+void calcTilt(sensors_vec_t* p_tilt, const sImu_t* p_imu, float f_dt)
+{
+    const sensors_vec_t* p_accl;
+    const sensors_vec_t* p_gyro;
+    sensors_vec_t tiltAccl;
+    sensors_vec_t tiltGyro;
+
+    p_accl = &(p_imu->accl.acceleration);
+    p_gyro = &(p_imu->gyro.gyro);
+
+    // get tilt from accelerometer
+    tiltAccl.roll    = atan2(p_accl->y,  RESULTANT(p_accl->x, p_accl->z));
+    tiltAccl.pitch   = atan2(-p_accl->x, RESULTANT(p_accl->y, p_accl->z));
+    // tiltAccl.heading = atan2(p_accl->z,  RESULTANT(p_accl->y, p_accl->x));
+
+    // get tilt from gyroscope
+    tiltGyro.roll    = p_tilt->roll    + p_gyro->x * f_dt;
+    tiltGyro.pitch   = p_tilt->pitch   + p_gyro->y * f_dt;
+    // tiltGyro.heading = p_tilt->heading + p_gyro->z * f_dt;
+
+    // sensor fusion using complementary filter
+    p_tilt->roll     = (TAU)*(tiltGyro.roll)    + (1-TAU)*(tiltAccl.roll);
+    p_tilt->pitch    = (TAU)*(tiltGyro.pitch)   + (1-TAU)*(tiltAccl.pitch);
+    // p_tilt->heading  = (TAU)*(tiltGyro.heading) + (1-TAU)*(tiltAccl.heading);
+
+    // heading (yaw) only from gyroscope
+    if (YAW_THRES < abs(p_gyro->z))
+    {
+        p_tilt->heading += p_gyro->z * f_dt;
+    }
+}
+
+String convJSON(const sensors_vec_t* p_tilt, const sImu_t* p_imu)
+{
+    const sensors_vec_t* p_accl;
+    const sensors_vec_t* p_gyro;
+    JSONVar data;
+
+    p_accl = &(p_imu->accl.acceleration);
+    p_gyro = &(p_imu->gyro.gyro);
+
+    data["temp"]      = String(p_imu->temp.temperature);
+    data["gyroX"]     = String(p_gyro->x);
+    data["gyroY"]     = String(p_gyro->y);
+    data["gyroZ"]     = String(p_gyro->z);
+    data["acclX"]     = String(p_accl->x);
+    data["acclY"]     = String(p_accl->y);
+    data["acclZ"]     = String(p_accl->z);
+    data["tiltYaw"]   = String(p_tilt->heading);
+    data["tiltRoll"]  = String(p_tilt->roll);
+    data["tiltPitch"] = String(p_tilt->pitch);
+
+    return JSON.stringify(data);
+}
+
+void calibrate(sImu_t* p_imu, uint32_t u32_sample)
+{
+    sSum_t* p_biasAccl;
+    sSum_t* p_biasGyro;
+    sImu_t tmpImu;
+    sSum_t sumAccl;
+    sSum_t sumGyro;
+
+    p_biasAccl = &(p_imu->biasAccl);
+    p_biasGyro = &(p_imu->biasGyro);
+
+    // reset to zero
+    memset(&sumAccl, 0x0, sizeof(sSum_t));
+    memset(&sumGyro, 0x0, sizeof(sSum_t));
+
+    // get a lot of sample
+    for(uint32_t u32_i = 0; u32_i < u32_sample; u32_i++) 
+    {
+        mpu.getEvent(&(tmpImu.accl), &(tmpImu.gyro), &(tmpImu.temp));
+
+        sumAccl.d_x += tmpImu.accl.acceleration.x;
+        sumAccl.d_y += tmpImu.accl.acceleration.y;
+        sumAccl.d_z += tmpImu.accl.acceleration.z;
+        sumGyro.d_x += tmpImu.gyro.gyro.x;
+        sumGyro.d_y += tmpImu.gyro.gyro.y;
+        sumGyro.d_z += tmpImu.gyro.gyro.z;
+
+        delay(1);
+    }
+
+    // calculcate baseline
+    p_biasAccl->d_x = sumAccl.d_x / u32_sample;
+    p_biasAccl->d_y = sumAccl.d_y / u32_sample;
+    p_biasAccl->d_z = sumAccl.d_z / u32_sample;
+    p_biasGyro->d_x = sumGyro.d_x / u32_sample;
+    p_biasGyro->d_y = sumGyro.d_y / u32_sample;
+    p_biasGyro->d_z = sumGyro.d_z / u32_sample;
+
+    // assume z-axis is perpendicular to earth gravity
+    p_biasAccl->d_z -= SENSORS_GRAVITY_STANDARD;
+}
+
+void measure(sImu_t* p_imu)
+{
+    // read sensor
+    mpu.getEvent(&(p_imu->accl), &(p_imu->gyro), &(p_imu->temp));
+
+    // get heatmap
+    p_imu->accl.acceleration.x -= p_imu->biasAccl.d_x;
+    p_imu->accl.acceleration.y -= p_imu->biasAccl.d_y;
+    p_imu->accl.acceleration.z -= p_imu->biasAccl.d_z;
+    p_imu->accl.gyro.x -= p_imu->biasGyro.d_x;
+    p_imu->accl.gyro.y -= p_imu->biasGyro.d_y;
+    p_imu->accl.gyro.z -= p_imu->biasGyro.d_z;
+}
+
+void report(const sensors_vec_t* p_tilt, const sImu_t* p_imu)
+{
+    // convert to json
+    const char* p_json = convJSON(p_tilt, p_imu).c_str();
+
+    // report to web
+    event.send(p_json, "readings", millis());
+
+    // report to oled
+    oled.clearDisplay();
+    oled.setCursor(0, 0);
+    oled.printf("%s:%d\n", WiFi.localIP().toString().c_str(), SVR_PORT);
+    oled.printf("Y: %3.2f deg\n", p_tilt->heading * SENSORS_RADS_TO_DPS);
+    oled.printf("R: %3.2f deg\n", p_tilt->roll    * SENSORS_RADS_TO_DPS);
+    oled.printf("P: %3.2f deg\n", p_tilt->pitch   * SENSORS_RADS_TO_DPS);
+    oled.display();
 }
