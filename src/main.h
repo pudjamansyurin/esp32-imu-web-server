9418bb3b01147d160ec3efea34b04ee24d8eaf6c
Puja Erawan <puja.erawan@viewtrixtech.com>
2023-02-18 13:30:03 +0700

    combine all sensor readings event

---
+++ src/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <Adafruit_Sensor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* exported typedefs ---------------------------------------------------------*/
+typedef struct
+{
+    double d_x;
+    double d_y;
+    double d_z;
+} sSum_t;
+
+typedef struct 
+{
+    sensors_event_t accl;       /* accelerometer */
+    sensors_event_t gyro;       /* gyroscope */
+    sensors_event_t temp;       /* temperature */
+    sSum_t biasAccl;
+    sSum_t biasGyro;
+} sImu_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_H_ */
